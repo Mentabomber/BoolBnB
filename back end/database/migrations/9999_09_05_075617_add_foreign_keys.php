6e988024d9faf8ca009944d7706d2fb5a8384a40
Mentabomber <mentae_mentology@libero.it>
2023-09-05 10:30:00 +0200

    Migrations down/up

--- back end/database/migrations/9999_09_05_075617_add_foreign_keys.php
+++ back end/database/migrations/9999_09_05_075617_add_foreign_keys.php
@@ -13,7 +13,26 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('apartments', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+        });
+        Schema::table('addresses', function (Blueprint $table) {
+                $table->foreignId('apartment_id')->constrained();
+        });
+        Schema::table('messages', function (Blueprint $table) {
+                $table->foreignId('apartment_id')->constrained();
+        });
+        Schema::table('visits', function (Blueprint $table) {
+                $table->foreignId('apartment_id')->constrained();
+        });
+        Schema::table('apartment_sponsorship', function (Blueprint $table) {
+                $table->foreignId('apartment_id')->constrained();
+                $table->foreignId('sponsorship_id')->constrained();
+        });
+        Schema::table('apartment_service', function (Blueprint $table) {
+                $table->foreignId('apartment_id')->constrained();
+                $table->foreignId('service_id')->constrained();
+        });
     }
 
     /**
@@ -23,6 +42,33 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('apartment_service', function (Blueprint $table) {
+            $table -> dropForeign('apartment_service_apartment_id_foreign');       
+            $table -> dropForeign('apartment_service_service_id_foreign');
+            $table -> dropColumn('apartment_id');
+            $table -> dropColumn('service_id');
+       });
+       Schema::table('apartments', function (Blueprint $table) {
+            $table -> dropForeign('apartments_user_id_foreign');
+            $table -> dropColumn('user_id');
+       });
+       Schema::table('addresses', function (Blueprint $table) {
+            $table -> dropForeign('addresses_apartment_id_foreign');
+            $table -> dropColumn('apartment_id');
+       });
+       Schema::table('messages', function (Blueprint $table) {
+            $table -> dropForeign('messages_apartment_id_foreign');
+            $table -> dropColumn('apartment_id');
+       });
+       Schema::table('visits', function (Blueprint $table) {
+            $table -> dropForeign('visits_apartment_id_foreign');
+            $table -> dropColumn('apartment_id');
+       });
+       Schema::table('apartment_sponsorship', function (Blueprint $table) {
+            $table -> dropForeign('apartment_sponsorship_apartment_id_foreign');
+            $table -> dropForeign('apartment_sponsorship_sponsorship_id_foreign');
+            $table -> dropColumn('apartment_id');
+            $table -> dropColumn('sponsorship_id');
+       });
     }
 };
