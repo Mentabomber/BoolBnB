ba579e1be45d645e90227b04893ac83f479382b9
DanieleCorrado <corrado_daniele@virgilio.it>
2023-09-12 15:08:52 +0200

    resolved full search

--- back end/resources/views/advanced-search.blade.php
+++ back end/resources/views/advanced-search.blade.php
@@ -1,25 +1,24 @@
 @extends('layouts.app')
 @section('content')
-
-<h1 class="display-5 fw-bold">
-    Ricerca Avanzata
-</h1>
-<form method="POST" action="{{ route('apartment.search') }}" enctype='multipart/form-data'>
-@csrf
-@method('POST')
+    <h1 class="display-5 fw-bold">
+        Ricerca Avanzata
+    </h1>
+    <form method="POST" action="{{ route('apartment.search') }}" enctype='multipart/form-data'>
+        @csrf
+        @method('POST')
+        <br>
+        <input type="hidden" name="address" id="resultField">
+        <input type="hidden" name="latitude" id="resultFieldLA">
+        <input type="hidden" name="longitude" id="resultFieldLO">
+        <br>
+        <label for="address">Indirizzo</label>
+        <br>
+        <input type="text" name="address" id="searchInput" placeholder="Cerca indirizzo">
+        <ul style="list-style-type: none;"id="suggestions"></ul>
+        <input class="my-3" type="submit" value="Cerca">
+    </form>
     <br>
-    <input type="hidden" name="address" id="resultField">
-    <input type="hidden" name="latitude" id="resultFieldLA">
-    <input type="hidden" name="longitude" id="resultFieldLO">
-    <br>
-    <label for="address">Indirizzo</label>
-    <br>
-    <input type="text" name="address" id="searchInput" placeholder="Cerca indirizzo">
-    <ul style="list-style-type: none;"id="suggestions"></ul>
-    <input class="my-3" type="submit" value="Cerca">
-</form>
-<br>
-{{-- <form method="POST" action="{{ route('apartment.search') }}" enctype='multipart/form-data'>
+    {{-- <form method="POST" action="{{ route('apartment.search') }}" enctype='multipart/form-data'>
 @csrf
 @method('POST') --}}
 
@@ -46,70 +45,76 @@
     @endforeach
     <br>
     <div>
-    <input class="my-3" type="submit" value="Cerca" id="bottoneInvio">
-{{-- </form> --}}
-    @foreach($apartments as $apartment)
-        <div id="apartment_card">
-            <a href="{{ route('guest.apartments.show', $apartment->id) }}">{{ $apartment->title }}</a>
-            <br>
-            <img src="{{ asset('storage/uploads/' . $apartment->image) }}" alt="">
-        </div>
-    @endforeach
-</div>
-
-<div class="content">
-    <div class="container">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
+        <input class="my-3" type="submit" value="Cerca" id="bottoneInvio">
+        {{-- </form> --}}
+        @foreach ($apartments as $apartment)
+            <div id="apartment_card">
+                <a href="{{ route('guest.apartments.show', $apartment->id) }}">{{ $apartment->title }}</a>
+                <br>
+                <img src="{{ asset('storage/uploads/' . $apartment->image) }}" alt="">
+            </div>
+        @endforeach
     </div>
-</div>
-<script type="text/javascript" src="{{ asset('assets/js/search-bar.js') }}"></script>
-<script>
-    const submit = document.getElementById("bottoneInvio");
-    submit.addEventListener("click", function() {
-        const beds = document.getElementById("available-beds");
-        const rooms = document.getElementById("available-rooms");
-        const apartment_card = document.getElementById("apartment_card");
-        const bedsValue = parseInt(beds.value);
-        const roomsValue = parseInt(rooms.value);
 
-        var beds_of_apartment = {!! json_encode($apartment->beds) !!};
-        var rooms_of_apartment = {!! json_encode($apartment->rooms) !!};
+    <div class="content">
+        <div class="container">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi
+                deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis
+                accusamus dolores!</p>
+        </div>
+    </div>
+    <script type="text/javascript" src="{{ asset('assets/js/search-bar.js') }}"></script>
+    <script>
+        const submit = document.getElementById("bottoneInvio");
+        submit.addEventListener("click", function() {
+            const beds = document.getElementById("available-beds");
+            const rooms = document.getElementById("available-rooms");
+            const apartment_card = document.getElementById("apartment_card");
+            const bedsValue = parseInt(beds.value);
+            const roomsValue = parseInt(rooms.value);
 
-        if (!isNaN(bedsValue) && !isNaN(roomsValue)) {
+            <?php
+            if (isset($apartment)) { ?>
+            var beds_of_apartment = {!! json_encode($apartment->beds) !!};
+            var rooms_of_apartment = {!! json_encode($apartment->rooms) !!};
 
-            if (!(bedsValue <= beds_of_apartment) || !(roomsValue <= rooms_of_apartment)) {
-                apartment_card.classList.add('hidden');
+            // applica i filtri sul numero di stanze e\o letti disponibili nell'appartamento
 
-            } else if ((bedsValue <= beds_of_apartment) && (roomsValue <= rooms_of_apartment)) {
-                apartment_card.classList.remove('hidden');
+            if (!isNaN(bedsValue) && !isNaN(roomsValue)) {
 
+                if (!(bedsValue <= beds_of_apartment) || !(roomsValue <= rooms_of_apartment)) {
+                    apartment_card.classList.add('hidden');
+
+                } else if ((bedsValue <= beds_of_apartment) && (roomsValue <= rooms_of_apartment)) {
+                    apartment_card.classList.remove('hidden');
+
+                }
+            } else if (!isNaN(bedsValue)) {
+                if (!(bedsValue <= beds_of_apartment)) {
+                    apartment_card.classList.add('hidden');
+
+                } else if (bedsValue <= beds_of_apartment) {
+                    apartment_card.classList.remove('hidden');
+
+                }
+            } else if (!isNaN(roomsValue)) {
+                console.log(rooms_of_apartment);
+                if (!(roomsValue <= rooms_of_apartment)) {
+                    apartment_card.classList.add('hidden');
+
+                } else if (roomsValue <= rooms_of_apartment) {
+                    apartment_card.classList.remove('hidden');
+
+                }
             }
-        } else if (!isNaN(bedsValue)) {
-            if (!(bedsValue <= beds_of_apartment)) {
-                apartment_card.classList.add('hidden');
+            <?php } ?>
 
-            } else if (bedsValue <= beds_of_apartment) {
-                apartment_card.classList.remove('hidden');
+        });
+    </script>
 
-            }
-        } else if (!isNaN(roomsValue)) {
-
-            if (!(roomsValue <= rooms_of_apartment)) {
-                apartment_card.classList.add('hidden');
-
-            } else if (roomsValue <= rooms_of_apartment) {
-                apartment_card.classList.remove('hidden');
-
-            }
+    <style>
+        .hidden {
+            display: none;
         }
-    });
-
-
-</script>
-
-<style>
-    .hidden {
-        display: none;
-    }
-</style>
+    </style>
 @endsection
